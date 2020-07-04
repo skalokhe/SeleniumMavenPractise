2f61a1e4cf6a1d0d5bbe2adfca985223c7c6a4e3
Sonal <sonalkalokhe.89@gmail.com>
2020-07-04 23:43:44 +0530

    SeleniumMavenPratise

---
+++ src/test/java/sonaMavenProject/Selenium.java
@@ -0,0 +1,19 @@
+package sonaMavenProject;
+
+import org.testng.annotations.Test;
+
+public class Selenium {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("Browser Automation");
+	}
+
+	@Test
+	public void UIAutomation()
+	{
+		System.out.println("UI Automation");
+	}
+
+}
---
+++ src/test/java/sonaMavenProject/mobile_Appium.java
@@ -0,0 +1,19 @@
+package sonaMavenProject;
+
+import org.testng.annotations.Test;
+
+public class mobile_Appium {
+	
+	@Test
+	public void Mlobile_01()
+	{
+		System.out.println("Browser Automation");
+	}
+
+	@Test
+	public void MobleAutomation()
+	{
+		System.out.println("UI Automation");
+	}
+
+}
